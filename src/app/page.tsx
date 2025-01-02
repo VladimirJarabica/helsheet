25c2480bab221f8f8e4afc81a54e7225a54d9db1
Vladimír Jarabica <vladojarabica@gmail.com>
2025-01-02 14:57:34 +0100

    remove console log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ export default async function Home() {
   const sheets = await dbClient.sheet.findMany({
     select: { id: true, name: true, Author: { select: { nickname: true } } },
   });
-  console.log("sheets", sheets);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
