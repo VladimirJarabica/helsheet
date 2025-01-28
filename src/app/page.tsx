35028941de6a33ba0265157a6e5d4617eef37481
Vladimír Jarabica <vladojarabica@gmail.com>
2025-01-28 15:02:40 +0100

    text sm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default async function Home() {
               <NewSheetButton />
             </div>
             <a
-              className="text-gray-500 inline-block mt-8"
+              className="text-gray-500 inline-block mt-8 text-sm"
               href="https://martincernansky.com/"
               target="_blank"
             >
